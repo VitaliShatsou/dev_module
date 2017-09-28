d9a5c80a78e894e9a5902c228356f9cc26c31e4c
VitaliShatsou <Vitali.Shatsou@thomsonreuters.com>
2017-09-28 15:23:35 +0300

    make it great again

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,43 @@
 
 var settingsPath = './settings.json';
-var fs = require('fs-extra');
-var settingsArr = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
-var exec = require('child_process').exec;
+const fs = require('fs-extra');
+const settingsArr = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
+const exec = require('child_process').exec;
 
 module.exports = function(grunt) {
-    grunt.registerTask('paste', function () {
-        settingsArr.forEach((settings) => {
-            const { dest, sourcemap } = settings;
-            sourcemap.files.forEach(file => {
-                const dirFile = sourcemap.dir + "/" + file;
-                const destFile = dest + file;
-                copyFile(dirFile, destFile);
-            });
-        }, this);
+    grunt.registerTask('paste', function (index) {
+        const settings = settingsArr[index];
+        const { dest, sourcemap } = settings;
+        sourcemap.files.forEach(file => {
+            const dirFile = sourcemap.dir + "/" + file;
+            const destFile = dest + file;
+            console.log(dirFile, destFile);
+            copyFile(dirFile, destFile);
+        });
     })
 
-    grunt.registerTask('gen', function () {
-        settingsArr.forEach(settings => {
-            const { root, sourcemap } = settings;
-            const { cmd } = sourcemap;
-            const done = this.async();
-            exec('cd ' + root + ' && ' + cmd, function (error, stdout, stderr) {
-                if(error) {
-                    console.log(error, stderr);
-                } else {
-                    console.log('succ');
-                }
-                done();
-            });
+    grunt.registerTask('gen', function (index) {
+        const settings = settingsArr[index];
+        const { root, sourcemap } = settings;
+        const { cmd } = sourcemap;
+        const done = this.async();
+        console.log('cd ' + root + ' && ' + cmd);
+        exec('cd ' + root + ' && ' + cmd, function (error, stdout, stderr) {
+            if(error) {
+                console.log(error, stderr);
+            } else {
+                console.log('succ');
+            }
+            done();
         });
-
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('watcher', function (index) {
-        // console.log(grunt.task.run);
         const files = settingsArr[index].watch + '**/*.js';
         grunt.config.set('watch', {
             files: [files],
-            tasks: ['gen', 'paste'],
+            tasks: ['gen:' + index + '', 'paste:' + index + ''],
         });
         grunt.task.run('watch');
     });
